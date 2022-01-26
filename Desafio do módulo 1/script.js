adf44fc9bce21e8a3fa18a3afd0fd0bdff954b8e
Samucael25 <s.samuelleonardo@gmail.com>
2022-01-25 23:59:50 -0300

    Buscando digitação do usuário

--- Desafio do módulo 1/script.js
+++ Desafio do módulo 1/script.js
@@ -1,2 +1,33 @@
-const dados = users;
-console.log (dados);
+let allPeople = null;
+
+window.addEventListener('load', () => {
+  readDataFromBackend();
+});
+
+
+async function readDataFromBackend() {
+  const resource = await fetch('http://localhost:3001/users');
+  const json = await resource.json();
+  allPeople = json.map (person => {
+    const { gender, name, picture } = person;
+    return {
+      gender,
+      nameComplete: name.first + ' ' + name.last,
+      picture
+    };
+  });
+  render();
+}
+
+function render(){
+  let inputSearch = document.querySelector ('#inputSearch');
+  inputSearch = document.addEventListener ('keyup',filteredList);
+}
+
+function filteredList(event){
+  let name = event.target.value;
+  let namesFind = allPeople.filter(person => {
+    return person.nameComplete.toLowerCase().includes(name.toLowerCase());
+  });
+  console.log(namesFind);
+}
