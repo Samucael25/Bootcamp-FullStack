e7cf6914a19b9d63a499225af888812dd1f680e5
Samucael25 <s.samuelleonardo@gmail.com>
2022-02-04 00:42:55 -0300

    Desafio finalizado com sucesso

--- Desafio do módulo 1/aplication/script.js
+++ Desafio do módulo 1/aplication/script.js
@@ -1,10 +1,9 @@
 let allPeople = null;
-const inputSearch = document.querySelector ('#inputSearch');
-const inputButton = document.querySelector ('#inputButton');
 const titleFindUsers = document.querySelector('#titleFindUsers');
 const titleStatistics = document.querySelector('#titleStatistics');
 const statistics = document.querySelector('#statistics');
 const findUsers = document.querySelector ('#findUsers');
+const miniListNames = document.querySelector ('#miniListNames');
 
 window.addEventListener('load', () => readDataFromBackend());
 
@@ -24,13 +23,16 @@ async function readDataFromBackend() {
 }
 
 function render(){
+  const inputSearch = document.querySelector ('#inputSearch');
+  const inputButton = document.querySelector ('#inputButton');
   startPage();
   inputSearch.addEventListener('keyup',(event) => {
     if (event.key === 'Enter') {
-      filteredList(inputSearch.value);
+      return filteredList(inputSearch.value, keyPressed = 'yes');
     }
+    return miniList(inputSearch.value, keyPressed = 'no');
   });
-  inputButton.addEventListener('click',(event) => filteredList(inputSearch.value));
+  return inputButton.addEventListener('click',(event) => filteredList(inputSearch.value, keyPressed = 'yes'));
 }
 
 function startPage(){
@@ -40,11 +42,34 @@ function startPage(){
   statistics.innerHTML = ``;
 }
 
-function filteredList(name){
+function miniList (name, keyPressed){
   let namesFind = allPeople.filter(person => {
     return person.nameComplete.toLowerCase().includes(name.toLowerCase());
   });
-  if (!((namesFind.length === 0) || (namesFind.length === allPeople.length) || (inputSearch.value === ''))){
+  if (keyPressed === 'yes') {
+    miniListNames.innerHTML = ``;
+    return namesFind;
+  }
+  showMiniList(namesFind);
+}
+
+function showMiniList (namesFindMinilist){
+  let namesMiniListHtml = '<div class="namesMiniList">';
+  namesFindMinilist.forEach(person => {
+    let peopleMiniListHtml = `
+      <div class="namesMinilist">
+        ${person.nameComplete}, 
+      </div>
+    `;
+    namesMiniListHtml += peopleMiniListHtml;
+  });
+  namesMiniListHtml += '</div>';
+  miniListNames.innerHTML = namesMiniListHtml;
+};
+
+function filteredList(name, keyPressed){
+  let namesFind = miniList(name, keyPressed);
+  if (!((namesFind.length === 0) || (namesFind.length === allPeople.length) || (inputSearch.value === ''))){    
     return showFilteredList(namesFind);
   }
   render();
@@ -56,7 +81,7 @@ function showFilteredList (namesFindList) {
   namesFindList.forEach(person => {
     let peopleHtml = `
       <div class="dataPerson">
-        <img src="${person.picture.thumbnail}" id="picture"> ${person.nameComplete}, ${person.age} anos
+        <img src="${person.picture.thumbnail}" id="picture"> <div class="nameAge">${person.nameComplete}, ${person.age} anos</div>
       </div>
     `;
     namesHtml += peopleHtml;
@@ -80,13 +105,13 @@ function calculateStatistics (namesForStatistics, countNames){
     return accumulator + current.age;
   },0);
 
-  let averageAge = sumAge/countNames;
+  let averageAge = (sumAge/countNames).toFixed(2);
 
   statistics.innerHTML = `
       <p id='countMale'>Sexo masculino: ${countMale}</p>
       <p id='countFemale'>Sexo feminino: ${countFemale}</p>
-      <p id='sumAge'>Soma das idades: ${sumAge}</p>
-      <p id='averageAge'>Média das idades: ${averageAge}</p>
+      <p id='sumAge'>Soma das idades: ${sumAge} anos</p>
+      <p id='averageAge'>Média das idades: ${averageAge} anos</p>
     `;
     modifyTitles (countNames);
 }
